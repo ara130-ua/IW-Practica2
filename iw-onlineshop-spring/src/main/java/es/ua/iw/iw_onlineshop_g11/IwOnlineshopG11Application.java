6f729b1338da1b624aada417a4f468cbf53d53be
50506576W Adrián Ríos Armero <ara130@alu.ua.es>
2024-11-19 19:49:10 +0100

    Añadidos los puntos de entrada de los dos proyectos

---
+++ iw-onlineshop-spring/src/main/java/es/ua/iw/iw_onlineshop_g11/IwOnlineshopG11Application.java
@@ -0,0 +1,13 @@
+package es.ua.iw.iw_onlineshop_g11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwOnlineshopG11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwOnlineshopG11Application.class, args);
+	}
+
+}
---
+++ iw-onlineshop-spring/src/test/java/es/ua/iw/iw_onlineshop_g11/IwOnlineshopG11ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ua.iw.iw_onlineshop_g11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwOnlineshopG11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
