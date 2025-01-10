b3d265d98e18067ddcf9f8d96ea3f2034a73008a
joan <joanclimentq@gmail.com>
2025-01-10 21:10:24 +0100

    añadida la conecion con la bbdd

--- iw-onlineshop-vue/src/main.js
+++ iw-onlineshop-vue/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import conexionbbdd from './conexionbbdd.js'
 
 import App from './App.vue'
 import router from './router'
@@ -10,3 +11,12 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// Connect to the database
+conexionbbdd.connect(function (error) {
+  if (error) {
+    throw error;
+  } else {
+    console.log('Conexión correcta con la base de datos.');
+  }
+});
